86b3f82b085dff01bb9a7ded39f51a383e4af8d2
Nicholas Elpidorou <P14138568@LEC-ADMIN.dmu.ac.uk>
2016-12-13 12:23:00 +0000

    end of lab

--- js/localstore.js
+++ js/localstore.js
@@ -4,12 +4,15 @@
   let getName = document.getElementById("get-name");
   let userName = document.getElementById("user-name"); 
   let nameStored = localStorage.name;
+  //
+  let lastNameStored = localStorage.lastName;
   console.log(`Name on page load: ${nameStored}`);
+  console.log(`Name on page load: ${lastNameStored}`);
   
   if(nameStored) {
     // If there's a name in localStorage, use it:
-    myName.innerHTML = `again ${nameStored}`;
-    console.log(`Name stored is: ${nameStored}`);
+    myName.innerHTML = `again ${nameStored} ${lastNameStored}`;
+    console.log(`Name stored is: ${nameStored} ${lastNameStored}`);
   }
   else {
     // There's no name in localStorage:
@@ -24,11 +27,13 @@
     }
     // Get the name the user entered:
     nameStored = userName.value;
+	lastNameStored = lastName.value;
     // Show the name in "my-name":
-    myName.innerHTML = nameStored;
+    myName.innerHTML = nameStored + " " + lastNameStored;
     // Put the name into localStorage:
     localStorage.name = nameStored;
-    console.log(`New name stored: ${nameStored}`);
+	localStorage.lastName = lastNameStored;
+    console.log(`New name stored: ${nameStored} ${lastNameStored}`);
     return false;
   }
 
@@ -40,5 +45,12 @@
     getName.addEventListener("submit", PerformGreeting);
     event.preventDefault();
   }
+  
+  function RemoveName(){
+	localStorage.clear();
+	
+  }
+  
+  document.getElementById("bye").onclick = RemoveName;
 
 }());
